808b494b803c9f4a7aa6e1cd26cbe3af69d0ba6a
archbelial <alpinakbar52@gmail.com>
2023-12-10 21:07:07 +0700

    second commit

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,16 +1,19 @@
 package com.example.demo.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-    public List<Student> GetStudents() {
-		return List.of(
-			new Student(1L, "Maria", "maria@gmail.com", LocalDate.of(2000, Month.JANUARY , 5), 23)
-		);
+
+	private final StudentRepository studentRepository; 
+
+    public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
+	public List<Student> GetStudents() {
+		return studentRepository.findAll();
 	}
 }
